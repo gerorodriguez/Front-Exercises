3f8d4f95c83c07602ce1c22db0b5c490f86a7ca7
manumrighi <manuelmorichi08@gmail.com>
2023-05-22 13:57:46 -0300

    consignas de ultimos 3 ejercicios

--- ReactEjercicios/src/components/login/Login.js
+++ ReactEjercicios/src/components/login/Login.js
@@ -1,5 +1,29 @@
 import { useState } from "react";
 
+// 3. Nuestro cliente, debido a traumas pasados, está en guerra abierta con todos los
+// usuarios que eligen como nombre de usuario una palabra que contenga la letra o
+// (sea mayúscula o minúscula).
+// Teniendo esto en cuenta, se nos ha pedido que creemos el componente Login.js que
+// contenga un único input donde el usuario ingresa su username y un botón que diga
+// Registrarse.
+// Funcionalidades:
+// ● El usuario ingresa su nombre de usuario y apreta en Registrarse. Si las
+// condiciones se cumplen exitosamente, le saldrá el mensaje con una alerta de
+// JavaScript que diga “¡Usuario registrado correctamente!”
+// ● Además, en un elemento <p> abajo del botón, se debe ir visualizando el valor
+// ingresado a medida que el usuario lo ingresa en el input.
+// Condiciones
+// ● Si el usuario en algún momento ingresa en el input una o/O se le debe
+// enviar una alerta de JavaScript que diga “Por favor, ¡Nombres de usuario sin
+// la letra o!”
+// ● Si al hacer click en registrarse, el nombre de usuario está vacío o posee la
+// letra o en su cadena de caracteres, enviar una alerta de JavaScript con el
+// mensaje “Usuario inválido para registrarse”
+// Pistas:
+// ● Método para la búsqueda de la letra o acá
+// ● Recordar buscar por mayúscula y por minúscula.
+// ● Recordar que el método includes() devuelve true o false.
+
 const Login = () => {
   const [userRegistered, setUserRegistered] = useState("");
   const handleChange = (event) => {
--- ReactEjercicios/src/components/tables/TableForm.js
+++ ReactEjercicios/src/components/tables/TableForm.js
@@ -1,6 +1,28 @@
 import React, { useState } from "react";
 import TableInput from "./TableInput";
 
+// 1. Deseamos crear dos componentes para el ingreso de la cantidad de patas que va a
+// tener una mesa. Los componentes se denominan TableForm y TableInput, y
+// necesitan cumplir los siguientes requisitos:
+// TableInput:
+// ● Este componente solo retornará un input en donde el usuario ingresará la
+// cantidad de patas que va a tener la mesa.
+// ● No es obligatorio pero se considerará puntos extra la buena utilización del
+// two-way data binding
+// TableForm:
+// ● Debe tener un mensaje h2 que indique “Ingrese la cantidad de patas que
+// tendrá la mesa”
+// ● Debe importar y utilizar el componente TableInput.
+// ● Debe tener un elemento p que:
+// ○ En el caso de que el usuario escriba el valor 4 en el input, mostrará el
+// mensaje “Mesa correcta”.
+// ○ Caso que no sea 4 el valor del input, mostrará “Mesa inestable”
+// Generales:
+// ● El componente debe ser funcional.
+// ● No es necesario ningún tipo de estilizado en la respuesta.
+// ● Solo escribir dos componentes
+// ● El único hook necesario para la resolución del ejercicio es useState
+
 const TableForm = () => {
   const [legsCount, setLegsCount] = useState("");
 
